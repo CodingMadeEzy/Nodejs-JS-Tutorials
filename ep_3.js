6fcfc89d1f640702d46098a0eef946bff704ef7e
Vipin Gupta <crushercandy95@gmail.com>
2024-02-22 21:39:20 +0530

    Episode 3 codes

---
+++ ep_3.js
@@ -0,0 +1,86 @@
+//Task solutions
+// function mohan() {
+//     console.log("I am cleaning the board");
+// }
+
+// function sohan() {
+//     console.log("I am bringing the chalks");
+// }
+
+// function monitor() {
+//     console.log("I am making class quiet");
+// }
+
+// //calling the functions
+// mohan();
+// sohan();
+// monitor();
+
+
+
+/////////////////////// OPERATORS //////////////////
+
+
+
+///////////////// Arithematic OPERATOR //
+var a = 10;
+var b = 20;
+
+// var c = a + b; //Addition
+// var c = a - b; //Subtraction
+// var c = a * b; //Multiplication
+// var c = a / b; //Division
+// var c = a % b; //Remainder/ modulus
+// var c = b % a; //Remainder/ modulus
+// var c = a++; //POST - Increment
+// var c = ++a; //PRE - Increment
+
+// var c = a--; //POST - Decrement
+// var c = --a; //PRE - Decrement
+
+// console.log(c);
+
+
+
+///////////////// Assignment OPERATOR //
+// var a = 10;//assignment operator (=)
+
+// a += 15; //addition operator
+// a -=5; //Subtraction operator
+// a *=5; //Multiplication operator
+// a /=5; //Division operator
+// a %=3; //Modulus operator
+
+// console.log(a);
+
+
+///////////////// Comparison OPERATOR //
+var a = 30;
+var b = 20;
+
+
+// var c = a == b; //equality operator
+
+//SPECIAL IMPORTANT
+// var d = 10;
+// var e = "10";
+
+// var f = d == e; //it checks only value
+// var g = d === e;//it checks value with datatype
+
+// console.log(f); 
+// console.log(g); 
+
+
+// var c = a != b;//not equals to
+// var c = a > b;//greater than
+// var c = a < b;//less than
+// var c = a >= b;//greater than or equal to
+// var c = a <= b;//less than or equal to
+
+// console.log(c);
+
+
+/// Create a program in which first ram buys 10 apples, multiply them by 3 and gives half of them to sohan, sohan gives half of his apples to ramesh and ramesh gives half of his apples to ram ,then how many apples ram,sohan and ramesh have ?
+
+
